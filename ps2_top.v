fbfcf967b6350c8fe0701eca549c2b85fe7348d0
jborza <jborza@gmail.com>
2020-10-31 20:53:10 +0100

    backspace implemented in top module

--- ps2_top.v
+++ ps2_top.v
@@ -11,7 +11,6 @@ module ps2_top (
 );
 
 // signals
-reg [1:0] state_reg, state_next;
 wire [7:0] scan_data;
 wire scan_done_tick;
 wire ps2_make_break;
@@ -23,14 +22,11 @@ reg [7:0] ram_in;
 wire [7:0] ram_out;
 wire [7:0] ascii_scan_data;
 reg [5:0] current_address;
-reg [5:0] current_address_next;
 
 // states
-localparam [1:0]
-	idle = 2'b00,
-	write_mem = 2'b01,
-	send1 = 2'b10,
-	done = 2'b11;
+	
+localparam BACKSPACE = 8'h08;
+localparam SPACE = 8'h20;
 
 ps2_keypress_driver ps2_keypress_driver_unit(
 	.clk(clk),
@@ -72,48 +68,32 @@ key2ascii key2ascii(
 //state registers
 always @(posedge clk, negedge reset)
 	if(~reset) begin
-		state_reg <= idle; //todo also clear the memory?
 		current_address <= 6'h0;
 	end else
 	begin
-		state_reg <= state_next;
-		current_address <= current_address_next;
+	  // on a key event:
+	  if(scan_done_tick) begin
+		 // key press
+		 if(ps2_make_break) begin
+			if(ascii_scan_data == BACKSPACE)	begin
+				write_address <= current_address - 1;
+				ram_in <= SPACE;
+				current_address <= current_address - 1;
+			end 
+			else begin //all other keys
+				write_address <= current_address;				
+				ram_in <= ascii_scan_data;	
+				current_address <= current_address + 1;
+			end
+			we <= 1'b1;
+		  end
+		end
+		else begin
+			we <= 1'b0;
+		end
 	end
 
-//next state logic
-always @*
-begin
-	state_next = state_reg;
-	we = 1'b0;
-	ram_in = 8'h3F;
-	write_address = 6'h0;
-	current_address_next = current_address;
-	
-	case(state_reg)
-	idle:
-		if(scan_done_tick) //scan code received, push to display
-		begin
-			state_next = write_mem;	
-		end
-	write_mem: //write scan code
-		begin
-			write_address = current_address;
-			ram_in = ascii_scan_data;
-			we = ps2_make_break; //1 only for make codes
-			state_next = done;
-		end
-	done: //extra tick 
-		begin
-			state_next = idle;
-			if(ps2_make_break) begin
-				current_address_next = current_address + 1;
-			end
-		end
-	endcase
-end
-
 assign led = ~scan_done_tick;
-assign state_led = ~state_reg;
 assign led_ps2c = ps2c;
 
 endmodule
